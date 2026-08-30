d601c5574c6fcd1835c4eb913a97cde0fdb9b3a9
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-08-30 21:09:59 +0100

    Update app.js
    Initializing the Map;
    render interactive map with dynamic backend facility markers

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+const map = L.map('map').setView([-8.8383, 13.2344], 12)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png' , {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map)
+
+
 async function fetchFacilities() {
     try {
         const response = await fetch('/api/facilities')
         const data = await response.json()
         console.log(data)
+
+        data.forEach(facility => {
+            L.marker([facility.lat, facility.lng])
+            .addTo(map)
+            .bindPopup(`<b>${facility.name}</b><br>${facility.municipality}`);
+        })
 
     } catch (error) {
         console.log('Error fetching data:' , error)
